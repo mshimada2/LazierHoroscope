e390fad1ac0a88c9163fff18d8dad9f52b873ea3
mshimada <mshimada2@mail.csuchico.edu>
2018-02-14 18:12:25 -0800

    still hate it

--- main.cpp
+++ main.cpp
@@ -26,29 +26,29 @@ int lucky;
 
 //tell fortune 
     
-    lucky = first.length() + last.length();
+    lucky = first.length();
 
     cout<<"Your lucky number is "<<lucky<<endl;
 
     if(first[0] == 'A' && first[0] == 'E' && first[0] == 'I' && first[0] == 'O' && first[0] == 'U' && first[0] == 'a' && first[0] == 'e' && first[0] == 'i' && first[0] == 'o' && first[0] == 'u'){
 
-    cout<<"you're destined to be famous! "<<endl; 
+    cout<<"you're destined to be famous!"<<endl; 
 
     }
 
    else{
 
-   cout<<"you should keep a low profile. "<<endl; 
+   cout<<"you should keep a low profile."<<endl; 
 
    }
 
    if(last[0] == 'a' && last[0] == 'e' && last[0] == 'i' && last[0] == 'o' && last[0] == 'u'){
 
-   cout<<"You have already met your true love. "<<endl;
+   cout<<"You have already met your true love."<<endl;
 
    }
 
-   cout<<"have a good day! "<<endl; 
+   cout<<"have a good day!"<<endl; 
 
 
   return 0;
